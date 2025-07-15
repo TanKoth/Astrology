67c6de21cae635b68c98b7d82e78de06b5c79677
TanmayKothale92 <tanmay.kothale@gmail.com>
2025-07-15 07:06:50 +0530

    install react-router-dom, axios, antd package. Created BrowserRouter for Home,Login,Register page in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+       <Routes>
+         <Route path={"/"} element = {<Home/>}/>
+         <Route path ={"/login"} element = {<Login/>} />
+         <Route path = {"/register"} element ={<Register/>} /> 
+        </Routes>
+      </BrowserRouter>
       
     </div>
   );
