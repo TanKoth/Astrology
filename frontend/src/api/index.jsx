170172affe740bd38312787aeb3d046ad96a4fb8
TanmayKothale92 <tanmay.kothale@gmail.com>
2025-08-11 02:35:29 +0530

    added the live url of server side to frontend

--- frontend/src/api/index.jsx
+++ frontend/src/api/index.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8081", // Adjust the base URL to your server's address
+  //baseURL: "http://localhost:8081", // Adjust the base URL to your server's address
+  baseURL: "https://vedic-vedang-ai.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
